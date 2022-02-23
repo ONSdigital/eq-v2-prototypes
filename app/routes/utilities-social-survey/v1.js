0c3c6b588e94786aac4b9d1eb2f9a35dc536cf9a
Jas Johal <JSJohal@users.noreply.github.com>
2022-02-23 14:22:48 +0000

    updated content

--- app/routes/utilities-social-survey/v1.js
+++ app/routes/utilities-social-survey/v1.js
@@ -39,9 +39,6 @@ module.exports = function (router) {
   // ******* HUB PAGE *******
   // **********************************************************************
   router.post(path + v + 'hub', function (req, res) {
-    if (req.session.data.reportingPeriodComplete !== true) {
-      res.redirect(path + v + guidance + 'how-to-complete')
-    }
     if (req.session.data.energyUtilitiesComplete !== true) {
       res.redirect(path + v + energyUtilities + 'total-spent-on-gas')
     }
@@ -53,8 +50,7 @@ module.exports = function (router) {
     }
     if (req.session.data.totalAmountComplete !== true) {
       res.redirect(path + v + totalAmount + 'complete')
-    }
-    else {
+    } else {
       res.redirect(path + v + 'complete')
     }
   })
@@ -65,61 +61,64 @@ module.exports = function (router) {
 
   // How to complete guidance page
   router.post(path + v + guidance + 'how-to-complete', function (req, res) {
-    // CTA redirects to 'are you able to report for the dates' question page
-    res.redirect(path + v + reportingPeriod + 'are-you-able-to-report')
+    // CTA redirects to 'hub' page
+    res.redirect(path + v + 'hub')
   })
+
+  // *************************************************** COMMENTED THIS SECTION OUT AS REDUNDANT ***************************************************
 
   // **********************************************************************
   // ******* REPORTING PERIOD SECTION *******
   // **********************************************************************
-
-  // 'Are you able to report for the dates' question page
-  router.post(path + v + reportingPeriod + 'are-you-able-to-report', function (req, res) {
-    // If user answers yes to 'able to report' question
-    if (req.session.data.ableToReport === 'yes') {
-      // Clear session flag for custom dates
-      req.session.data.customDates = null
-      // Clear custom dates if already set previously
-      req.session.data.whatDateFromDay = null
-      req.session.data.whatDateFromMonth = null
-      req.session.data.whatDateFromMonthText = null
-      req.session.data.whatDateFromYear = null
-      req.session.data.whatDateToDay = null
-      req.session.data.whatDateToMonth = null
-      req.session.data.whatDateToMonthText = null
-      req.session.data.whatDateToYear = null
-      // CTA redirects to the reporting period CYA page
-      res.redirect(path + v + reportingPeriod + 'check-your-answers')
-    }
-    // If user answers no to 'able to report' question
-    if (req.session.data.ableToReport === 'no') {
-      // CTA redirects to the 'what dates can you report for' question page
-      res.redirect(path + v + reportingPeriod + 'what-dates-will-you-be-reporting-for')
-    }
-  })
-
-  // 'What dates will you be reporting for?' question page
-  router.post(path + v + reportingPeriod + 'what-dates-will-you-be-reporting-for', function (req, res) {
-    // Set session flag for custom dates
-    req.session.data.customDates = true
-
-    // Convert month number to text string for the date from
-    req.session.data.whatDateFromMonthText = monthNumToName(req.session.data.whatDateFromMonth)
-
-    // Convert month number to text string for the date to
-    req.session.data.whatDateToMonthText = monthNumToName(req.session.data.whatDateToMonth)
-
-    // CTA redirects to the reporting period CYA page
-    res.redirect(path + v + reportingPeriod + 'check-your-answers')
-  })
-
-  // Reporting period check your answers page
-  router.post(path + v + reportingPeriod + 'check-your-answers', function (req, res) {
-    // Mark section as complete
-    req.session.data.reportingPeriodComplete = true
-    // CTA redirects to 'hub' page
-    res.redirect(path + v + 'hub')
-  })
+  //
+  // // 'Are you able to report for the dates' question page
+  // router.post(path + v + reportingPeriod + 'are-you-able-to-report', function (req, res) {
+  //   // If user answers yes to 'able to report' question
+  //   if (req.session.data.ableToReport === 'yes') {
+  //     // Clear session flag for custom dates
+  //     req.session.data.customDates = null
+  //     // Clear custom dates if already set previously
+  //     req.session.data.whatDateFromDay = null
+  //     req.session.data.whatDateFromMonth = null
+  //     req.session.data.whatDateFromMonthText = null
+  //     req.session.data.whatDateFromYear = null
+  //     req.session.data.whatDateToDay = null
+  //     req.session.data.whatDateToMonth = null
+  //     req.session.data.whatDateToMonthText = null
+  //     req.session.data.whatDateToYear = null
+  //     // CTA redirects to the reporting period CYA page
+  //     res.redirect(path + v + reportingPeriod + 'check-your-answers')
+  //   }
+  //   // If user answers no to 'able to report' question
+  //   if (req.session.data.ableToReport === 'no') {
+  //     // CTA redirects to the 'what dates can you report for' question page
+  //     res.redirect(path + v + reportingPeriod + 'what-dates-will-you-be-reporting-for')
+  //   }
+  // })
+  //
+  // // 'What dates will you be reporting for?' question page
+  // router.post(path + v + reportingPeriod + 'what-dates-will-you-be-reporting-for', function (req, res) {
+  //   // Set session flag for custom dates
+  //   req.session.data.customDates = true
+  //
+  //   // Convert month number to text string for the date from
+  //   req.session.data.whatDateFromMonthText = monthNumToName(req.session.data.whatDateFromMonth)
+  //
+  //   // Convert month number to text string for the date to
+  //   req.session.data.whatDateToMonthText = monthNumToName(req.session.data.whatDateToMonth)
+  //
+  //   // CTA redirects to the reporting period CYA page
+  //   res.redirect(path + v + reportingPeriod + 'check-your-answers')
+  // })
+  //
+  // // Reporting period check your answers page
+  // router.post(path + v + reportingPeriod + 'check-your-answers', function (req, res) {
+  //   // Mark section as complete
+  //   req.session.data.reportingPeriodComplete = true
+  //   // CTA redirects to 'hub' page
+  //   res.redirect(path + v + 'hub')
+  // })
+  // *************************************************** END COMMENTED OUT SECTION ***************************************************
 
   // **********************************************************************
   // ******* ENERGY UTILITIES *******
